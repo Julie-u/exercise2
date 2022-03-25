79b1062c2177b46f9c8194f5801e75499fba4afa
julie ugolotti <julie.ugolotti@gmail.com>
2022-03-25 19:16:05 +0100

    ex2

---
+++ js/account.js
@@ -0,0 +1,77 @@
+function buttonSuccess(){
+    if(document.getElementById("firstname").value === '' || document.getElementById("lastname").value === ''){
+      document.getElementById("update").disabled = true;
+    }
+    else {
+      document.getElementById("update").disabled = false;
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+      const firstname = document.getElementById("firstname");
+      const lastname = document.getElementById("lastname");
+      const update = document.getElementById("update");
+  
+      const db = firebase.firestore();
+  
+      let userRef = null;
+  
+      const toggleButton = document.getElementById('toggle-account');
+      const navLinks = document.getElementById('nav-links-account');
+  
+      toggleButton.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+      })
+  
+      update.addEventListener("click", function (event) {
+        event.preventDefault()
+        if (firstname.value && lastname.value) {
+          updateUser(userRef.uid, firstname.value, lastname.value);
+        }
+      });
+  
+      function updateUser(uid, first, last) {
+        const p = document.getElementById('account-text');
+        db.collection("Users")
+          .doc(uid)
+          .update({
+            firstname: first,
+            lastname: last,
+          })
+          .then(function () {
+            p.style.display = "block"
+            p.innerHTML = `User updated. Upload files <a href="index.html">here</a>`
+            console.log("User updated!");
+          })
+          .catch(function (error) {
+            p.style.display = "block"
+            p.innerHTML = `${error}`
+            console.error(error);
+          });
+      }
+  
+      function getUser(uid) {
+        const p = document.getElementById('account-text');
+        db.collection("Users")
+          .doc(uid)
+          .get()
+          .then(function (doc) {
+            firstname.value = doc.data().firstname;
+            lastname.value = doc.data().lastname;
+          })
+          .catch(function (error) {
+            p.style.display = "block"
+            p.innerHTML =  `${error}`
+            console.error(error);
+          });
+      }
+  
+      firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+          userRef = user;
+          getUser(user.uid);
+        } else {
+          window.location = "login.html";
+        }
+      });
+    });
---
+++ js/register.js
@@ -0,0 +1,67 @@
+function buttonSuccess(){
+    if(document.getElementById("firstname").value === '' || document.getElementById("lastname").value === '' || document.getElementById("email").value === '' || document.getElementById("password").value === ''){
+      document.getElementById("register").disabled = true;
+    }
+    else {
+      document.getElementById("register").disabled = false;
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+      const firstname = document.getElementById("firstname");
+      const lastname = document.getElementById("lastname");
+      const email = document.getElementById("email");
+      const password = document.getElementById("password");
+      const register = document.getElementById("register");
+  
+      const db = firebase.firestore();
+  
+      const toggleButton = document.getElementById('toggle-register');
+      const navLinks = document.getElementById('nav-links-register');
+  
+      toggleButton.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+      })
+  
+  
+  
+      function addUser(uid, first, last) {
+        db.collection("Users")
+          .doc(uid)
+          .set({
+            firstname: first,
+            lastname: last,
+            user: uid,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          })
+          .then(function () {
+            document.getElementById("registration-text").style.display = "block"
+            document.getElementById("registration-text").innerHTML = `Registeration successful. Upload <a class="registration-text-link" href="index.html">here</a>`
+            console.log("User added to database!");
+          })
+          .catch(function (error) {
+            document.getElementById("registration-text").style.display = "block"
+            document.getElementById("registration-text").innerHTML = `${error}`;
+            console.error(error);
+          });
+      }
+  
+      register.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (email.value && password.value) {
+          firebase
+            .auth()
+            .createUserWithEmailAndPassword(email.value, password.value)
+            .then(function (data) {
+              const user = firebase.auth().currentUser;
+  
+              addUser(user.uid, firstname.value, lastname.value);
+            })
+            .catch(function (error) {
+              document.getElementById("registration-text").style.display = "block"
+              document.getElementById("registration-text").innerHTML = `${error}`;
+              console.error(error);
+            });
+        }
+      });
+    });
